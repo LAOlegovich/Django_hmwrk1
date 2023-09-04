a7d5bce6ee3a70fa9bf640ce3d8ef6204c5daaca
Neto proj <lit25111987@gmail.com>
2023-09-04 22:34:14 +0300

    Paginator csv

--- 1.2-requests-templates/pagination/stations/views.py
+++ 1.2-requests-templates/pagination/stations/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
+from django.core.paginator import Paginator
+import csv
 
 
 def index(request):
@@ -7,11 +9,20 @@ def index(request):
 
 
 def bus_stations(request):
+    page_number = request.GET.get("page",1)
+    with open('data-398-2018-08-30.csv', newline='', encoding='utf-8') as csvfile:
+        data = csv.DictReader(csvfile)
+        list_of_data = [row for row in data]
+    print(list_of_data)
+
+    pagi = Paginator(list_of_data,20)
+    page = pagi.get_page(int(page_number))
+            
     # получите текущую страницу и передайте ее в контекст
     # также передайте в контекст список станций на странице
 
     context = {
-    #     'bus_stations': ...,
-    #     'page': ...,
+         'bus_stations':page,
+         'page': page
     }
     return render(request, 'stations/index.html', context)
