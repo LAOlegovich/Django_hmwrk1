cd62053630e30d51c23094db60dd788531a07c0a
Neto proj <lit25111987@gmail.com>
2023-09-09 23:58:21 +0300

    ORM1_commit

--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -1,6 +1,20 @@
 from django.db import models
+from django.urls import reverse
+
 
 
 class Phone(models.Model):
     # TODO: Добавьте требуемые поля
-    pass
+    id = models.IntegerField(primary_key = True, db_index = True)
+    name = models.CharField(max_length=50, unique = True, verbose_name= 'Имя')
+    price = models.FloatField(verbose_name = 'Цена',)
+    image = models.CharField(max_length=100, verbose_name= 'Ссылка')
+    release_date = models.DateField(verbose_name = 'Дата выпуска')
+    lte_exists = models.BooleanField(verbose_name = 'Наличие LTE')
+    slug = models.SlugField(max_length=100, unique=True, db_index=True, verbose_name="URL")
+
+    def get_p(self,par):
+        if par == "name":
+            return self.name
+        else:
+            return self.price
--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-
+from phones.models import Phone
 
 def index(request):
     return redirect('catalog')
@@ -7,11 +7,19 @@ def index(request):
 
 def show_catalog(request):
     template = 'catalog.html'
-    context = {}
+    data = Phone.objects.all()
+    par = request.GET.get("sort","name")
+    match par:
+        case "name": f = False
+        case "min_price":f = False
+        case "max_price": f = True
+    data_sorted= sorted(list(data), key = lambda x:x.get_p(par),reverse = f)
+    context = {'phones':data_sorted}
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
+    data = Phone.objects.get(slug = slug)
+    context = {'phone':data}
     return render(request, template, context)
