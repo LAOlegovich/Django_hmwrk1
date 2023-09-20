316e47e01a6f4c9ba52f2cf03f7e694133d3ae1e
Neto proj <lit25111987@gmail.com>
2023-09-20 23:07:23 +0300

    my_commit

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,35 @@
 from django.contrib import admin
 
-from .models import Article
+from .models import Article, Scope
 
+from django.forms import BaseInlineFormSet
+
+from django.core.exceptions import ValidationError
+
+class ScopeInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        i = 0
+        for form in self.forms:
+            # В form.cleaned_data будет словарь с данными
+            # каждой отдельной формы, которые вы можете проверить
+            i += int(form.cleaned_data['is_main'])
+            if i > 1:
+                raise ValidationError('Может быть только одна основная категория!')
+
+            # вызовом исключения ValidationError можно указать админке о наличие ошибки
+            # таким образом объект не будет сохранен,
+            # а пользователю выведется соответствующее сообщение об ошибке
+            
+        return super().clean()  # вызываем базовый код переопределяемого метода
+
+class RelationshipInline(admin.TabularInline):
+    model = Scope
+    formset = ScopeInlineFormset
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    pass
+    inlines = [RelationshipInline]
+
+
+
+
